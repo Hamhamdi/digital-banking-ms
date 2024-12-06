4e539958422d719a0bcc9a66e0ba8711c2e3ec36
hamza <hamdihamza.aui@gmail.com>
2024-12-06 21:53:10 +0100

    feat: add statuses (Pending, Executed, Canceled) for transfers to enhance business logic

---
+++ virement-service/src/main/java/net/hamdi/virementservice/enums/VirementStatus.java
@@ -0,0 +1,7 @@
+package net.hamdi.virementservice.enums;
+
+public enum VirementStatus {
+    PENDING,    // Transfer is pending
+    EXECUTED,   // Transfer executed
+    CANCELLED   // Transfer canceled
+}
