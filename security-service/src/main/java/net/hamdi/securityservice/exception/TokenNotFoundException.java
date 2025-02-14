bb9b3f8bde344b46c0b671b91972d23ba9896797
hamza <hamdihamza.aui@gmail.com>
2025-02-14 15:20:49 +0100

    refactor: removed security service for migration to Keycloak

--- security-service/src/main/java/net/hamdi/securityservice/exception/TokenNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package net.hamdi.securityservice.exception;
-
-public class TokenNotFoundException extends RuntimeException{
-    public TokenNotFoundException(String message) {
-        super(message);
-    }
-}
